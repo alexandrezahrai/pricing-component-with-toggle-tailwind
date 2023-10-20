496830c2f49d11151efa4b5259a31addbeda022f
Alex <alexandrezahrai@protonmail.com>
2023-10-19 22:11:32 -0400

    store the monthly and yearly price values in separate arrays

--- public/index.js
+++ public/index.js
@@ -3,6 +3,8 @@ const priceSwitch = document.querySelector("input[type='checkbox']");
 const basicPrice = document.getElementById("basic-price");
 const proPrice = document.getElementById("pro-price");
 const masterPrice = document.getElementById("master-price");
+const monthlyPrices = ["$19.99", "$24.99", "$39.99"];
+const yearlyPrices = ["$240", "$300", "$480"];
 
 // Event listener(s)
 priceSwitch.addEventListener("change", updatePricing);
@@ -12,12 +14,12 @@ function updatePricing() {
   let isChecked = priceSwitch.checked;
 
   if (isChecked) {
-    basicPrice.innerHTML = "$19.99";
-    proPrice.innerHTML = "$24.99";
-    masterPrice.innerHTML = "$39.99";
+    basicPrice.innerHTML = monthlyPrices[0];
+    proPrice.innerHTML = monthlyPrices[1];
+    masterPrice.innerHTML = monthlyPrices[2];
   } else {
-    basicPrice.innerHTML = "$240.00";
-    proPrice.innerHTML = "$300.00";
-    masterPrice.innerHTML = "$480.00";
+    basicPrice.innerHTML = yearlyPrices[0];
+    proPrice.innerHTML = yearlyPrices[1];
+    masterPrice.innerHTML = yearlyPrices[2];
   }
 }
